461e4af48b97e0c3d9d72e5c19ee27e4825e4e91
Almaz <Iheardthatwas@gmail.com>
2023-12-23 17:55:09 +0500

    test

--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -1,4 +1,4 @@
-"use client";
+import { GetServerSideProps } from "next";
 import Image from "next/image";
 import { useEffect, useState } from "react";
 interface Product {
@@ -10,51 +10,18 @@ interface Product {
   description: string;
   image: string;
 }
-const ProductSkeleton = () => {
-  return (
-    <div className="border border-gray-200 shadow rounded-md p-4 max-w-sm w-full mx-auto">
-      <div className="animate-pulse flex space-x-4">
-        <div className="rounded-full bg-slate-200 h-10 w-10"></div>
-        <div className="flex-1 space-y-6 py-1">
-          <div className="h-2 bg-slate-200 rounded"></div>
-          <div className="space-y-3">
-            <div className="grid grid-cols-3 gap-4">
-              <div className="h-2 bg-slate-200 rounded col-span-2"></div>
-              <div className="h-2 bg-slate-200 rounded col-span-1"></div>
-            </div>
-            <div className="h-2 bg-slate-200 rounded"></div>
-          </div>
-        </div>
-      </div>
-    </div>
-  );
-};
 
-export default function Example() {
-  const [data, setData] = useState<Product[]>([]);
-  const [isLoading, setIsLoading] = useState(true);
+async function getData() {
+  const res = await fetch("http://localhost:3001/api/products");
 
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const res = await fetch("http://localhost:3001/api/products");
-        const result = await res.json();
-        if (res.ok) {
-          setData(result.data);
-        }
-      } catch (error) {
-        console.error("Error fetching data:", error);
-      } finally {
-        setIsLoading(false);
-      }
-    };
+  if (!res.ok) {
+    throw new Error("Failed to fetch data");
+  }
 
-    fetchData();
-  }, []);
-
-  useEffect(() => {
-    console.log(data);
-  }, [data]);
+  return res.json();
+}
+export default async function Products() {
+  const products: { data: Product[] } = await getData();
 
   return (
     <div className="bg-white">
@@ -62,47 +29,40 @@ export default function Example() {
         <h2 className="sr-only">Products</h2>
 
         <div className="grid grid-cols-1 gap-y-4 sm:grid-cols-2 sm:gap-x-6 sm:gap-y-10 lg:grid-cols-3 lg:gap-x-8">
-          {isLoading
-            ? Array(6)
-                .fill(0)
-                .map((_, index) => <ProductSkeleton key={index} />)
-            : data.length > 0 &&
-              data.map((product) => (
-                <div
-                  key={product.id}
-                  className="group relative flex flex-col overflow-hidden rounded-lg border border-gray-200 bg-white"
-                >
-                  <div className="aspect-h-4 aspect-w-3 bg-gray-200 sm:aspect-none group-hover:opacity-75 sm:h-96">
-                    <Image
-                      src={product.image}
-                      alt={product.image}
-                      width={0}
-                      height={0}
-                      unoptimized
-                      className="h-full w-full object-cover object-center sm:h-full sm:w-full"
-                    />
-                  </div>
-                  <div className="flex flex-1 flex-col space-y-2 p-4">
-                    <h3 className="text-sm font-medium text-gray-900">
-                      <a href={product.link}>
-                        <span aria-hidden="true" className="absolute inset-0" />
-                        {product.title}
-                      </a>
-                    </h3>
-                    <p className="text-sm text-gray-500">
-                      {product.description}
-                    </p>
-                    <div className="flex flex-1 flex-col justify-end">
-                      <p className="text-sm italic text-gray-500">
-                        {product.brand}
-                      </p>
-                      <p className="text-base font-medium text-gray-900">
-                        {product.price} ₸
-                      </p>
-                    </div>
-                  </div>
+          {products.data.map((product) => (
+            <div
+              key={`${product.id}-${product.title}`}
+              className="group relative flex flex-col overflow-hidden rounded-lg border border-gray-200 bg-white"
+            >
+              <div className="aspect-h-4 aspect-w-3 bg-gray-200 sm:aspect-none group-hover:opacity-75 sm:h-96">
+                <Image
+                  src={product.image}
+                  alt={product.image}
+                  width={0}
+                  height={0}
+                  unoptimized
+                  className="h-full w-full object-cover object-center sm:h-full sm:w-full"
+                />
+              </div>
+              <div className="flex flex-1 flex-col space-y-2 p-4">
+                <h3 className="text-sm font-medium text-gray-900">
+                  <a href={product.link}>
+                    <span aria-hidden="true" className="absolute inset-0" />
+                    {product.title}
+                  </a>
+                </h3>
+                <p className="text-sm text-gray-500">{product.description}</p>
+                <div className="flex flex-1 flex-col justify-end">
+                  <p className="text-sm italic text-gray-500">
+                    {product.brand}
+                  </p>
+                  <p className="text-base font-medium text-gray-900">
+                    {product.price} ₸
+                  </p>
                 </div>
-              ))}
+              </div>
+            </div>
+          ))}
         </div>
       </div>
     </div>
