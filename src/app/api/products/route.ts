7968fac952e4caf52732d864d6d2ba0474b1bfda
Almaz <Iheardthatwas@gmail.com>
2023-12-23 19:05:54 +0500

    test

--- src/app/api/products/route.ts
+++ src/app/api/products/route.ts
@@ -1,5 +1,5 @@
 import { Client } from "@notionhq/client";
-import { NextRequest, NextResponse } from "next/server";
+import { NextResponse } from "next/server";
 
 const notionSecret = process.env.NOTION_SECRET!;
 const notionDatabaseId = process.env.NOTION_DATABASE_ID!;
