e9489849b3041a289bc5e5cc447e22c39d12b096
Almaz <Iheardthatwas@gmail.com>
2023-12-23 18:03:13 +0500

    url fix

--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -10,7 +10,7 @@ interface Product {
 }
 
 async function getData() {
-  const res = await fetch("http://localhost:3000/api/products");
+  const res = await fetch(process.env.URL + "/api/products");
 
   if (!res.ok) {
     throw new Error("Failed to fetch data");
