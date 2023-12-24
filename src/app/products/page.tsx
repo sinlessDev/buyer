bd4eedf57efe0d719f41c8f113540dfc731efc81
Almaz <Iheardthatwas@gmail.com>
2023-12-24 23:38:13 +0500

    test

--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -25,7 +25,7 @@ export default async function Products() {
   return (
     <div className="bg-white">
       <div className="mx-auto max-w-2xl px-4 py-16 sm:px-6 sm:py-24 lg:max-w-7xl lg:px-8">
-        <h2 className="sr-only">Products</h2>
+        <h2 className="sr-only">Продукты</h2>
 
         <div className="grid grid-cols-1 gap-y-4 sm:grid-cols-2 sm:gap-x-6 sm:gap-y-10 lg:grid-cols-3 lg:gap-x-8">
           {products.data.map((product) => (
